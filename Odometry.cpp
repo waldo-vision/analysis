ce4fc43c2a95c7da180298c97a7a6a0c559b0bac
DPS100 <0dps100@gmail.com>
2021-10-31 22:29:09 -0600

    Code cleanup & refactoring

--- Odometry.cpp
+++ Odometry.cpp
@@ -31,9 +31,13 @@ using namespace cv;
  * @param[out] points1 Array to send keypoints to
  * @return Points of interest
  */
-std::vector<Point2f> featureDetection(const Mat& imgIn, std::vector<KeyPoint>& pointsOut, int threshold = 30);
+void featureDetection(const Mat& imgIn, std::vector<Point2f>& pointsOut, int threshold);
 
 void featureTracking(const Mat& img_1, const Mat& img_2, std::vector<Point2f>& points1, std::vector<Point2f>& points2, std::vector<uchar>& status);
+
+void getPoseChange(std::vector<Point2f>& firstPoints, std::vector<Point2f>& secondPoints, const double& focal, const Point2d& principalPoint, Mat& R, Mat& t);
+
+void updateAndTrack(const Mat& firstFrame, const Mat& secondFrame, std::vector<Point2f>& firstPoints, std::vector<Point2f>& secondPoints, std::vector<uchar>& status, bool redetect = true, int threshold = 10);
 
 int main(int argc, char** argv) {
 
@@ -44,7 +48,7 @@ int main(int argc, char** argv) {
         return 1;
     }
     String fileIn(argv[1]);
-    int threshold = 30;
+    int threshold = 10;
     if(argc >= 3) {
         threshold = std::stoi(argv[2]);
     }
@@ -55,126 +59,70 @@ int main(int argc, char** argv) {
 
     // Open video
     VideoCapture cap(fileIn);
-    Mat out;
-    Mat grayLast;
-    cap >> out;
-    int rows = out.rows;
-    int cols = out.cols;
-    Mat movementMap = Mat::zeros(rows, cols, out.type());
-    rectangle(movementMap, Rect(0, 0, cols/5, cols/5), Scalar(255,255,255), -1);
+    Mat vidFrame, prevFrame;
+    cap >> vidFrame;
+    prevFrame = vidFrame.clone();
+    // FIXME test
+    int rows = vidFrame.rows;
+    int cols = vidFrame.cols;
     VideoWriter vidOut;
-    vidOut.open("out.mp4", VideoWriter::fourcc('m','p','4','v'), 24, out.size(), true);
-    std::vector<KeyPoint> keypoints;
-    std::vector<Point2f> points;
-    std::vector<Point2f> prevPoints;
+    vidOut.open("out.mp4", VideoWriter::fourcc('m','p','4','v'), 24, vidFrame.size(), true);
+    std::vector<Point2f> points, prevPoints;
     std::vector<uchar> status;
-    int minGoodPoints = 1600 / threshold; // TODO this is arbitrary, may need tuning (Lower threshold = higher good points needed)
-
-    double sumX = 0;
-    double sumY = 0;
-    double sumZ = 0;
-
-    Mat totalR = Mat::zeros(3, 3, CV_64FC1);
+    int minGoodPoints = 2000; // TODO this is arbitrary, may need tuning (Lower threshold = higher good points needed)
+    std::cout << "Video open\n";
 
     double focal = 100;
     Point2d opticalCenter(cols / 2, rows / 2);
 
+    // Find t, R between first two frames
+    Mat globalR, globalt;
+    cap.read(prevFrame);
+    cap.read(vidFrame);
+    updateAndTrack(prevFrame, vidFrame, prevPoints, points, status, true, threshold);
+    getPoseChange(points, prevPoints, focal, opticalCenter, globalR, globalt);
+
+
     // Main loop
     int frame = 0;
-    int sumGoodPoints = 0;
     time_t timeStart = time(NULL);
-    while(cap.read(out) && vidOut.isOpened()) {
-        // Detect features
-        Mat grayOut;
-        cvtColor(out, grayOut, COLOR_BGR2GRAY);
+    while(cap.read(vidFrame) && vidOut.isOpened()) {        
+        // Get feature movement (optical flow)
+        updateAndTrack(vidFrame, prevFrame, points, prevPoints, status, prevPoints.size() < minGoodPoints, threshold);
 
-        // Refresh points below a certain detection threshold (Unused, tends to track UI)
+        // Normalize feature movement RANSAC and get pose change
+        Mat R, t;
+        getPoseChange(points, prevPoints, focal, opticalCenter, R, t);
+        // TODO test if the order of these vvv helps drift
+        globalR = globalR * R;
+        globalt = globalt + globalR * t;
 
-        if(sumGoodPoints < minGoodPoints) {
-            //std::cout << "Points at " << keypoints.size() << " Good points at " << sumGoodPoints <<  ": refreshing\n";
-            points = featureDetection(grayOut, keypoints, threshold);
-        }
-        sumGoodPoints = 0;
-        
-        // Track features
-        if(prevPoints.size() != 0) { // Not first frame
-            featureTracking(grayOut, grayLast, points, prevPoints, status);
+        Mat display = vidFrame.clone();
 
-            Mat E, mask;
-            E = findEssentialMat(points, prevPoints, focal, opticalCenter, LMEDS, 0.999, 1.0, mask);
-            Mat R, t;
-            if(E.size().area() == 0) {
-                std::cout << mask.size().area();
-                std::cout << "Essential matrix is empty\n";
-            } else {
-                recoverPose(E, points, prevPoints, R, t, focal, opticalCenter, mask);  
-
-                // Count valid points
-                for(int i = 0; i < mask.size().area(); i++) {
-                    if(mask.at<bool>(i)) sumGoodPoints++;
-                }
-
-                // Strange output when the same image is presented
-                // https://stackoverflow.com/questions/33372310/why-does-opencvs-recoverpose-return-a-non-origin-position-when-identical-point
-                // Unit is (may need to test on different systems) = 0.5773502691896257
-                if(t.at<double>(0) == 0.5773502691896257) {
-                    t =  Mat::zeros(t.rows, t.cols, t.type()); 
-                } else {
-                    // Use this for global tracking, otherwise t is relative to last frame
-                    t = t + totalR * t;
-                    // FIXME - Need to correct for drift when movement is low
-                    // Right now there the number of points persists fairly regular with low movement, so low discard rates probably mean no movement
-                    sumX += t.at<double>(0);
-                    sumY += t.at<double>(1);
-                    sumZ += t.at<double>(2);
-                    // Short term movement
-                    rectangle(movementMap, Rect((cols / 5), 0, cols/10, cols/10), Scalar(255,255,255), -1);
-                    circle(movementMap, Point(cols / 20 + cols / 5 + t.at<double>(0) * 10, cols / 20 + t.at<double>(1) * 10), 0, Scalar(255, 0, 0), 50);
-
-                    // http://planning.cs.uiuc.edu/node103.html
-                    // 0 1 2
-                    // 3 4 5
-                    // 6 7 8
-                    // alpha(yaw) = arctan(r3 / r0)
-                    // beta(pitch) = arctan(- r6 * sqrt(r7 ^ 2 + r8 ^ 2))
-                    // gamma(roll) = arctan(r7 / r8)
-                    double alpha = atan(R.at<double>(3) / R.at<double>(0));
-                    Mat rod;
-                    totalR = totalR * R;
-                    Rodrigues(totalR, rod);
-                    if(debug) std::cout << "Rodriguez: " << rod << '\n';
-                }
-
-                // Long term tracker
-                circle(movementMap, Point((cols / 2 + sumX * 3) / 5, (cols / 2 + sumY * 3) / 5), 1, Scalar(255, 0, 0));
-
-            }
+        // Draw points to output
+        for(int i = 0; i < points.size() && i < prevPoints.size(); i++) {
+            line(display, points.at(i), prevPoints.at(i), Scalar::all(255), 2);
         }
 
-        //std::cout << "X travelled: " << sumX << " Y travelled: " << sumY << " Z travelled" << sumZ << '\n';
-
-        // Put last frame data in storage
-        prevPoints = std::vector<Point2f>(points);
-        grayLast = grayOut;
-
-        // Add player movement map to video
-        add(movementMap, out, out);
+        // Put this frames data into storage
+        prevPoints = points;
+        prevFrame = vidFrame.clone();
 
         // Show output (Only with debug)
         if (debug) {
-            imshow("Data", out);
+            imshow("Data", display);
             char c = waitKey(0);
             if(c == 'q' || c == 'Q') break;
         }
 
         // Write output
         if(!debug && frame % 25 == 0) { // Make output less cluttered
-            std::cout << "Frame " << frame << " good points " << sumGoodPoints << '\n';
+            std::cout << "Frame " << frame << '\n';
         } else if(debug) {
-            std::cout << "Frame " << frame << " good points " << sumGoodPoints << " points " << points.size() << '\n';
+            std::cout << "Frame " << frame << " points " << points.size() << '\n';
         }
         frame++;
-        vidOut.write(out);
+        vidOut.write(display);
     }
 
     // Close program
@@ -183,14 +131,14 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-std::vector<Point2f> featureDetection(const Mat& imgIn, std::vector<KeyPoint>& pointsOut, int threshold)	{ 
+void featureDetection(const Mat& imgIn, std::vector<Point2f>& pointsOut, int threshold)	{ 
     bool nonmaxSuppression = true;
-    FAST(imgIn, pointsOut, threshold, nonmaxSuppression);
-    std::vector<Point2f> points(0);
-    for(KeyPoint p : pointsOut) {
-        points.push_back(p.pt);
+    std::vector<KeyPoint> keypoints(0);
+    FAST(imgIn, keypoints, threshold, nonmaxSuppression);
+    pointsOut.clear();
+    for(KeyPoint p : keypoints) {
+        pointsOut.push_back(p.pt);
     }
-    return points;
 }
 
 void featureTracking(const Mat& img_1, const Mat& img_2, std::vector<Point2f>& points1, std::vector<Point2f>& points2, std::vector<uchar>& status)	{ 
@@ -202,6 +150,11 @@ void featureTracking(const Mat& img_1, const Mat& img_2, std::vector<Point2f>& p
     TermCriteria termcrit=TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
 
     cv::calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
+
+    for(int i = 0; i < points1.size(); i++) {
+        if(points1.at(i) != points2.at(i)) {
+        }
+    }
 
     //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
     int indexCorrection = 0;
@@ -216,4 +169,37 @@ void featureTracking(const Mat& img_1, const Mat& img_2, std::vector<Point2f>& p
             indexCorrection++;
         }
     }
+}
+
+void getPoseChange(std::vector<Point2f>& firstPoints, std::vector<Point2f>& secondPoints, const double& focal, const Point2d& principalPoint, Mat& R, Mat& t) {
+    // Find essential matrix
+    Mat mask, E;
+    E = findEssentialMat(firstPoints, secondPoints, focal, principalPoint, RANSAC, 0.999, 1.0, mask);
+    // Recover pose
+    recoverPose(E, firstPoints, secondPoints, R, t, focal, principalPoint, mask);
+    // Remove outliers
+    // TODO this will not be implimented until a recoverPose() workaround for no motion is found.
+    // int removed = 0;
+    // for(int i = 0; i < mask.size().area(); i++) {
+    //     if(!mask.at<bool>(i)) {
+    //         firstPoints.erase(firstPoints.begin() + i - removed);
+    //         secondPoints.erase(secondPoints.begin() + i - removed);
+    //         removed++;
+    //     }
+    // }
+}
+
+void updateAndTrack(const Mat& firstFrame, const Mat& secondFrame, std::vector<Point2f>& firstPoints, std::vector<Point2f>& secondPoints, std::vector<uchar>& status, bool redetect, int threshold) {
+    // Grayscale frames
+    Mat firstFrameG; cvtColor(firstFrame, firstFrameG, COLOR_BGR2GRAY);
+    Mat secondFrameG; cvtColor(secondFrame, secondFrameG, COLOR_BGR2GRAY);
+    // Find from this frame
+    if(redetect) {
+        featureDetection(firstFrameG, firstPoints, threshold);
+        std::cout << "Redetect triggered\n";
+    }
+    // Track first to second
+    
+    if(firstFrame.at<double>(20000) == secondFrame.at<double>(20000)) std::cout << "WARNING using same frames in before tracking\n";
+    featureTracking(firstFrameG, secondFrameG, firstPoints, secondPoints, status);
 }
